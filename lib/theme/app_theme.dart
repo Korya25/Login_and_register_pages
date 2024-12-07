f1d41a3a27cf84ce32746d42a9a80d82759a1947
Korya25 <enthrj3@gmail.com>
2024-12-08 00:28:14 +0200

    crrate a app_theme File contain lightMode and DarkMode

---
+++ lib/theme/app_theme.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+// Light Theme
+ThemeData lightMode = ThemeData(
+  colorScheme: ColorScheme.light(
+    surface: Colors.grey.shade300,
+    primary: Colors.grey.shade500,
+    secondary: Colors.grey.shade200,
+    tertiary: Colors.white,
+    inversePrimary: Colors.grey.shade900,
+  ),
+);
+
+// Dark Theme
+ThemeData darkMode = ThemeData(
+  colorScheme: ColorScheme.dark(
+    surface: Colors.grey.shade800,
+    primary: Colors.grey.shade600,
+    secondary: Colors.grey.shade700,
+    tertiary: Colors.black,
+    inversePrimary: Colors.grey.shade200,
+  ),
+);
