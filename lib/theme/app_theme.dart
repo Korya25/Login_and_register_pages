92702b2e9ba755e9516df677c32cb453e4556c5e
Korya25 <enthrj3@gmail.com>
2024-12-08 00:47:31 +0200

    add MultiBlocProvider   to main Function and add Bloc Builder To MyApp

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -1,23 +1,25 @@
 import 'package:flutter/material.dart';
 
+class AppTheme {
 // Light Theme
-ThemeData lightMode = ThemeData(
-  colorScheme: ColorScheme.light(
-    surface: Colors.grey.shade300,
-    primary: Colors.grey.shade500,
-    secondary: Colors.grey.shade200,
-    tertiary: Colors.white,
-    inversePrimary: Colors.grey.shade900,
-  ),
-);
+  ThemeData lightMode = ThemeData(
+    colorScheme: ColorScheme.light(
+      surface: Colors.grey.shade300,
+      primary: Colors.grey.shade500,
+      secondary: Colors.grey.shade200,
+      tertiary: Colors.white,
+      inversePrimary: Colors.grey.shade900,
+    ),
+  );
 
 // Dark Theme
-ThemeData darkMode = ThemeData(
-  colorScheme: ColorScheme.dark(
-    surface: Colors.grey.shade800,
-    primary: Colors.grey.shade600,
-    secondary: Colors.grey.shade700,
-    tertiary: Colors.black,
-    inversePrimary: Colors.grey.shade200,
-  ),
-);
+  ThemeData darkMode = ThemeData(
+    colorScheme: ColorScheme.dark(
+      surface: Colors.grey.shade800,
+      primary: Colors.grey.shade600,
+      secondary: Colors.grey.shade700,
+      tertiary: Colors.black,
+      inversePrimary: Colors.grey.shade200,
+    ),
+  );
+}
