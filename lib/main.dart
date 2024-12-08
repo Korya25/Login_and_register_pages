9fed446ab8b8927d788f87f7bc34ee7c991385aa
Korya25 <enthrj3@gmail.com>
2024-12-08 04:02:30 +0200

    add firebase core and firebase auth packeges to projest                          import firebase to may main app

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:login_and_register_pages/firebase_options.dart';
 import 'package:login_and_register_pages/logic/theme/theme_cuibt.dart';
 import 'package:login_and_register_pages/my_app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(
     MultiBlocProvider(
       providers: [BlocProvider(create: (context) => ThemeCuibt())],
